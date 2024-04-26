46302e66c6c8f79b5220780c1f780ff2431a1d04
vedant Yevatikar <bichkundevedant97@gmail.com>
2024-04-27 03:36:51 +0530

    search navigation and popup sign in page added

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 
+// JS code for home page
+
+var section1=document.getElementById("section1");
+var section2=document.getElementById("section2");
+var section3=document.getElementById("section3");
+
+var sectionBlock1=document.getElementById("sectionBlock1");
+var sectionBlock2=document.getElementById("sectionBlock2");
+var sectionBlock3=document.getElementById("sectionBlock3");
+
+section1.onclick=function(){
+    sectionBlock1.style.display="block";
+    sectionBlock2.style.display="none";
+    sectionBlock3.style.display="none";
+}
+section2.onclick=function(){
+    sectionBlock2.style.display="block";
+    sectionBlock1.style.display="none";
+    sectionBlock3.style.display="none";
+}
+section3.onclick=function(){
+    sectionBlock3.style.display="block";
+    sectionBlock1.style.display="none";
+    sectionBlock2.style.display="none";
+}
 
 // Autocomplete search box code
 
@@ -9,15 +34,17 @@
         var $input = $(".typeahead"); 
         $input.typeahead({ 
             source: [ 
-                "Cloud computing", 
+                "ChatGPT",
+                "Graphic Designing",
+                "Digital marketing",
                 "Video editing", 
-                "Machine learning", 
-                "Introduction to programming", 
-                "Introduction to  cyber security", 
-                "Digital marketing", 
+                "Machine learning",
+                "Cloud computing", 
+                "Introduction to SQL",
                 "Data analysis", 
-                "ChatGPT", 
-                "Introduction to SQL", 
+                "Introduction to  cyber security",
+                "Artificial Intelligence",
+                "Introduction to programming",
             ], 
             autoSelect: true, 
         }); 
@@ -39,6 +66,73 @@
 
 // End
 
+// Search box navigation code
+
+//start
+
+var searchBox=document.getElementById("inputBox");
+searchBox.addEventListener('keypress', (e) =>{
+
+if(e.key=='Enter')
+{
+    var val=searchBox.value;
+
+    if(val=='ChatGPT')
+    {
+        window.location.href = "chatGPT.html"; 
+    }
+    else if(val=='Graphic Designing')
+    {
+        window.location.href = "graphicDesigning.html";
+    }
+    else if(val=='Digital marketing')
+    {
+        window.location.href = "digitalMarketing.html";
+    }
+    else if(val=='Video editing')
+    {
+        window.location.href = "videoEditing.html";
+    }
+    else if(val=='Machine learning')
+    {
+        window.location.href = "machineLearning.html";
+    }
+    else if(val=='Cloud computing')
+    {
+        window.location.href = "cloudComputing.html";
+    }
+    else if(val=='Introduction to SQL')
+    {
+        window.location.href = "introductionToSQL.html";
+    }
+    else if(val=='Data analysis')
+    {
+        window.location.href = "dataAnalyst.html";
+    }
+    else if(val=='Introduction to  cyber security')
+    {
+        window.location.href = "cyberSecurity.html";
+    }
+    else if(val=='Artificial Intelligence')
+    {
+        window.location.href = "artificialIntelligence.html";
+    }
+    else if(val=='Introduction to programming')
+    {
+        window.location.href = "programming.html";
+    }
+
+}
+
+});
+
+//end
+
+
+// Sign In Page code
+
+//Start
+
 let signupBtn = document.getElementById("signupBtn");
 let signinBtn = document.getElementById("signinBtn");
 let namefield = document.getElementById("namefield");
@@ -51,8 +145,32 @@ signinBtn.onclick =function(){
     signinBtn.classList.remove("disable")
 }
 signupBtn.onclick =function(){
-    namefield.style.maxHeight = "60px";
+    namefield.style.maxHeight = "35px";
     title.innerHTML="sign Up";
     signupBtn.classList.remove("disable");
     signinBtn.classList.add("disable")
 }
+
+//End
+
+// Code to popup a sign In Page
+
+var signInButton1=document.getElementById("signInButton");
+var signInContainer1=document.querySelector(".SignInContainer");
+
+signInButton1.onclick=function(){
+
+    // sectionBlock1.style.display="none";
+    // signInContainer1.style.backgroundColor="black";
+    signInContainer1.style.display="block";
+
+}
+
+var crosIcon=document.getElementById("crosIcon");
+
+crosIcon.onclick=function(){
+
+    signInContainer1.style.display="none";
+}
+
+
